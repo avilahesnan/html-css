685b9fc0da91267257cf07af56af2d5961cf66ba
Hesnan Ávila <avilahesnan@outlook.com>
2023-12-28 15:46:07 -0300

    new file: alurabooks api

---
+++ mini-projects/alurabooks-api/js/script.js
@@ -0,0 +1,28 @@
+const endPointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+const elementInsertBooks = document.querySelector('#livros');
+let books = [];
+
+async function getSearchBooks() {
+    const response = await fetch(endPointAPI);
+    books = await response.json();
+    console.table(books);
+    showBooks(books);
+}
+
+function showBooks(listBooks) {
+    listBooks.forEach(book => {
+        elementInsertBooks.innerHTML += `
+        <div class="livro">
+            <img class="livro__imagens" src="${book.imagem}" alt="${book.alt}" />
+            <h2 class="livro__titulo">${book.titulo}</h2>
+            <p class="livro__descricao">${book.autor}</p>
+            <p class="livro__preco" id="preco">${book.preco}</p>
+            <div class="tags">
+                <span class="tag">${book.categoria}</span>
+            </div>
+        </div>
+        `
+    });
+}
+
+getSearchBooks();
